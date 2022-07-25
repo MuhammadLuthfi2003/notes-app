08bfb55d4d19707224bae9a33abe9a6ab0cab075
MuhammadLuthfi2003 <muhammad.luthfi2003@mail.ugm.ac.id>
2022-07-25 16:34:04 +0700

    added div for later additions

--- src/components/activeNoteComponent.js
+++ src/components/activeNoteComponent.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function activeNoteComponent({id, title, body, createdAt}) {
+    return (
+        <div>
 
+        </div>
+    )
 }
 
 export default activeNoteComponent;
--- src/components/archivedNoteComponent.js
+++ src/components/archivedNoteComponent.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function ArchivedNoteComponent({id, title, body, createdAt}) {
+    return (
+        <div>
 
+        </div>
+    )
 }
 
 export default ArchivedNoteComponent;
